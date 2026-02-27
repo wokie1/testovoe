bb8dd682e841df1a228d05edd763e7d23ff6df71
wockie1 <s.wokki.ie@gmail.com>
2026-02-27 04:14:12 +0400

    fix home_img

--- src/components/CurrentModels.jsx
+++ src/components/CurrentModels.jsx
@@ -3,7 +3,7 @@ import getImgPath from "../getImgPath";
 const CurrentModels = ({ imgUrl, title }) => {
     return (
         <div className="model-card">
-            <img src={`/images${getImgPath(imgUrl)}`} alt="image" />
+            <img src={`/images${getImgPath(imgUrl)}`} alt="image h-100" />
             <h2 className="title">{title}</h2>
             <div className="btn-container">
                 <button className="btn">ПОДРОБНЕЕ</button>
